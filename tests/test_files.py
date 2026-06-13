a60928f8cf5693fcfe256356443ede246f00662e
Pavan Kumar Balusupati <66351065+pavan1613@users.noreply.github.com>
2026-06-13 10:48:58 +0530

    Update test_files.py
    Added updated test_files.py file

--- tests/test_files.py
+++ tests/test_files.py
@@ -3,10 +3,15 @@ from pathlib import Path
 def test_css_exists():
     assert Path("styles.css").exists()
 
+def test_image_exists():
+    assert Path("1000687159.png").exists()
+
 def test_video_exists():
     assert Path(
         "PixVerse_V6_Image_Text_360P_Create_self_intro_.mp4"
     ).exists()
 
-def test_image_exists():
-    assert Path("1000687159.png").exists()
+def test_resume_exists():
+    assert Path(
+        "PavanKumar_Python_Automation_Resume.pdf"
+    ).exists()
